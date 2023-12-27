fb1ed4ac7651bb00f1d3db1c8d376683182f737a
deveix <devx@duoera.com>
2023-12-28 00:34:55 +0200

    fix for next config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  webpack: (config, {isServer}) => {
-    if (!isServer) {
-      // Exclude the folder from being processed in the client-side bundle
-      config.module.rules.push({
-        test: /functions/,
-        loader: "null-loader", // A loader that does nothing
-      });
-    }
-    return config;
-  },
+  exclude: ["functions"],
 };
-
-module.exports = nextConfig;
