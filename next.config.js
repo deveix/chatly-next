4cfdb6d9d7ee35b327f40bf4da3531baff39e9bd
deveix <devx@duoera.com>
2023-12-28 00:30:03 +0200

    skip functions folder

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+module.exports = {
+  webpack: (config, {isServer}) => {
+    if (!isServer) {
+      // Exclude the folder from being processed in the client-side bundle
+      config.module.rules.push({
+        test: /functions/,
+        loader: "null-loader", // A loader that does nothing
+      });
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
